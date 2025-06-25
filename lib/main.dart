1cd4afa073215a14fe6d98835c6262489bd1645a
Jhony Galerani <jhony.galerani@outlook.com>
2025-06-25 18:40:05 -0300

    AJUSTADO A COR DO APPBAR E NOME + A ESCRITA DO TEXTO CENTRAL

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Dashboard Financeiro', // Atualizado o titulo do aplicativo
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -28,9 +28,9 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Dashboard Financeiro'), // atualizado o titulo do home
     );
   }
 }
@@ -104,12 +104,18 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            const Text(
+             'Voce apertou este botão esta quantidade de vezes:',
+             style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+             ),
+            ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
-          ],
+            ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
